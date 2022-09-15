092a31c6d10c61fe0a1d9575673d2ae7a401daca
jeanpiress <jean.m.pires@gmail.com>
2022-09-14 22:23:54 -0300

    findById implementatio and reburn

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import db.DB;
+import model.dao.impl.SellerDaoJdbc;
+
+public class DaoFactory {
+
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJdbc(DB.getConnection());
+	}
+}
